01fbfac097d4dedf9a1278a9edc32e7c57526339
Furkan Kambay <furkankambay@protonmail.com>
2023-03-23 12:34:40 +0300

    Improve ground checking

--- Assets/Scripts/CharacterController/GroundChecker.cs
+++ Assets/Scripts/CharacterController/GroundChecker.cs
@@ -7,24 +7,19 @@ namespace Game.CharacterController
         public bool IsOnGround { get; private set; }
 
         [SerializeField] private float groundLength = .5f;
-        [SerializeField] private float colliderOffset = .5f;
         [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private Vector2 boxSize;
 
-        private Vector3 LeftSide => transform.position - (Vector3.right * colliderOffset);
-        private Vector3 RightSide => transform.position + (Vector3.right * colliderOffset);
-
-        private void Update()
+        private void FixedUpdate()
         {
-            IsOnGround =
-                Physics2D.Raycast(LeftSide, Vector2.down, groundLength, groundLayer)
-                || Physics2D.Raycast(RightSide, Vector2.down, groundLength, groundLayer);
+            IsOnGround = Physics2D.BoxCast(transform.position, boxSize, 0,
+                Vector2.down, groundLength, groundLayer);
         }
 
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = IsOnGround ? Color.green : Color.red;
-            Gizmos.DrawLine(LeftSide, LeftSide + (Vector3.down * groundLength));
-            Gizmos.DrawLine(RightSide, RightSide + (Vector3.down * groundLength));
+            Gizmos.DrawWireCube(transform.position, boxSize);
         }
     }
 }
