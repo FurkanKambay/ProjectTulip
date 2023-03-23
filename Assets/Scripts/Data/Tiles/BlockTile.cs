a148c534f39a44fcb17495d55c9391f1b355df2d
Furkan Kambay <furkankambay@protonmail.com>
2023-03-23 12:03:51 +0300

    Play block hit sounds

--- Assets/Scripts/Data/Tiles/BlockTile.cs
+++ Assets/Scripts/Data/Tiles/BlockTile.cs
@@ -1,4 +1,5 @@
-﻿using Game.Data.Items;
+﻿using System.Collections.Generic;
+using Game.Data.Items;
 using UnityEngine;
 
 namespace Game.Data.Tiles
@@ -6,20 +7,25 @@ namespace Game.Data.Tiles
     [CreateAssetMenu(fileName = "Block", menuName = "Items/Block")]
     public class BlockTile : CustomTile, IUsable
     {
+        public float UseTime => .25f;
+
         [Header("Data")]
         public int hardness = 50;
+        public AudioClip hitSound;
 
-        public float UseTime => .25f;
+        private static Dictionary<Vector3Int, int> DamageMap => World.Instance.TileDamageMap;
 
         public void Use(Vector3Int cellPosition)
             => World.Instance.Tilemap.SetTile(cellPosition, this);
 
         public void GetHit(int damage, Vector3Int cell)
         {
-            if (!World.Instance.TileDamageMap.ContainsKey(cell))
-                World.Instance.TileDamageMap.Add(cell, 0);
+            if (!DamageMap.ContainsKey(cell))
+                DamageMap.Add(cell, 0);
 
-            int damageTaken = World.Instance.TileDamageMap[cell] += damage;
+            AudioSource.PlayClipAtPoint(hitSound, World.Instance.Tilemap.CellToWorld(cell));
+
+            int damageTaken = DamageMap[cell] += damage;
             if (damageTaken >= hardness)
                 World.Instance.Tilemap.SetTile(cell, null);
         }
