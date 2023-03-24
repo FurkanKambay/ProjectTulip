3c648bebfd8669a755f5e788aabc218b6a046f72
Furkan Kambay <furkankambay@protonmail.com>
2023-03-24 23:37:53 +0300

    Increase ground check performance

--- Assets/Scripts/CharacterController/GroundChecker.cs
+++ Assets/Scripts/CharacterController/GroundChecker.cs
@@ -6,15 +6,11 @@ namespace Game.CharacterController
     {
         public bool IsOnGround { get; private set; }
 
-        [SerializeField] private float groundLength = .5f;
         [SerializeField] private LayerMask groundLayer;
         [SerializeField] private Vector2 boxSize;
 
-        private void FixedUpdate()
-        {
-            IsOnGround = Physics2D.BoxCast(transform.position, boxSize, 0,
-                Vector2.down, groundLength, groundLayer);
-        }
+        private void FixedUpdate() => IsOnGround = Physics2D.OverlapBox(
+            transform.position, boxSize, 0, groundLayer);
 
         private void OnDrawGizmosSelected()
         {
