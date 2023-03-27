0ae0485a9d069c2811c5eb2db7326b0e61c044bf
Furkan Kambay <furkankambay@protonmail.com>
2023-03-27 07:19:35 +0300

    Add player melee animation

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using Game.Data.Items;
+using UnityEngine;
 
 namespace Game.Player
 {
@@ -6,18 +8,32 @@ namespace Game.Player
     {
         private static readonly int animSpeed = Animator.StringToHash("speed");
         private static readonly int animJump = Animator.StringToHash("jump");
+        private static readonly int animAttack = Animator.StringToHash("attack");
+        private static readonly int animAttackSpeed = Animator.StringToHash("attack speed");
 
         private Animator animator;
+        private Inventory inventory;
 
-        private void Awake() => animator = GetComponent<Animator>();
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            inventory = GetComponentInParent<Inventory>();
+        }
+
+        private void Start()
+        {
+            inventory.HotbarSelectionChanged += _ => OnHotbarSelection(inventory.HotbarSelected);
+            OnHotbarSelection(inventory.HotbarSelected);
+        }
+
+        private void OnHotbarSelection(IUsable item)
+            => animator.SetFloat(animAttackSpeed, 1 / item.UseTime);
 
         private void Update()
         {
-            float moveX = Input.Actions.Player.MoveX.ReadValue<float>();
-            animator.SetFloat(animSpeed, Mathf.Abs(moveX));
-
-            bool jump = Input.Actions.Player.Jump.triggered;
-            animator.SetBool(animJump, jump);
+            animator.SetFloat(animSpeed, Mathf.Abs(Input.Actions.Player.MoveX.ReadValue<float>()));
+            animator.SetBool(animJump, Input.Actions.Player.Jump.inProgress);
+            animator.SetBool(animAttack, Input.Actions.Player.Fire.inProgress);
         }
     }
 }
