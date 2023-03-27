e212f68b0d30dcf6d2ed9f1c9baf8eb2ad9924d2
Furkan Kambay <furkankambay@protonmail.com>
2023-03-27 06:34:02 +0300

    Add player animations

---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Game.Player
+{
+    public class PlayerAnimator : MonoBehaviour
+    {
+        private static readonly int animSpeed = Animator.StringToHash("speed");
+        private static readonly int animJump = Animator.StringToHash("jump");
+
+        private Animator animator;
+
+        private void Awake() => animator = GetComponent<Animator>();
+
+        private void Update()
+        {
+            float moveX = Input.Actions.Player.MoveX.ReadValue<float>();
+            animator.SetFloat(animSpeed, Mathf.Abs(moveX));
+
+            bool jump = Input.Actions.Player.Jump.triggered;
+            animator.SetBool(animJump, jump);
+        }
+    }
+}
