05defda62196484168ccf8a45ce3bffc02d724a1
Furkan Kambay <furkankambay@protonmail.com>
2023-03-27 07:15:52 +0300

    Change Inventory Start to Awake and invoke event in OnEnable

--- Assets/Scripts/Player/Inventory.cs
+++ Assets/Scripts/Player/Inventory.cs
@@ -26,18 +26,23 @@ namespace Game.Player
             HotbarSelectionChanged?.Invoke(index);
         }
 
-        private void Start()
+        private void Awake()
         {
             Hotbar[0] = Resources.Load<Pickaxe>("Tools/Pickaxe");
             Hotbar[1] = Resources.Load<BlockTile>("Tiles/Stone");
             Hotbar[2] = Resources.Load<BlockTile>("Tiles/Dirt");
-            Hotbar[3] = Resources.Load<BlockTile>("Tiles/Sand");
+            Hotbar[3] = Resources.Load<BlockTile>("Tiles/Sandstone");
             Hotbar[4] = Resources.Load<BlockTile>("Tiles/Ice");
 
             HotbarModified?.Invoke();
         }
 
-        private void OnEnable() => Input.Instance.HotbarSelected += OnHotbarSelected;
+        private void OnEnable()
+        {
+            Input.Instance.HotbarSelected += OnHotbarSelected;
+            HotbarSelectionChanged?.Invoke(hotbarSelectedIndex);
+        }
+
         private void OnDisable() => Input.Instance.HotbarSelected -= OnHotbarSelected;
     }
 }
