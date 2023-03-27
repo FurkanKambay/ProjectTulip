6a24282ff14c498837acd58caece6d8411e74d0a
Furkan Kambay <furkankambay@protonmail.com>
2023-03-27 17:48:26 +0300

    Create custom rule tile script

--- Assets/Scripts/Data/Tiles/BlockTile.cs
+++ Assets/Scripts/Data/Tiles/BlockTile.cs
@@ -1,13 +1,14 @@
 ﻿using Game.Data.Items;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace Game.Data.Tiles
 {
     [CreateAssetMenu(fileName = "Block", menuName = "Items/Block")]
-    public class BlockTile : CustomTile, IUsable
+    public class BlockTile : RuleTile<BlockTile.Neighbor>, IUsable
     {
         public float UseTime => .25f;
-        public Sprite Icon => sprite;
+        public Sprite Icon => m_DefaultSprite;
 
         [Header("Data")]
         public int hardness = 50;
@@ -15,5 +16,18 @@ namespace Game.Data.Tiles
         [Header("Sounds")]
         public AudioClip hitSound;
         public AudioClip placeSound;
+
+        public override bool RuleMatch(int neighbor, TileBase tile) => neighbor switch
+        {
+            TilingRuleOutput.Neighbor.NotThis => tile == null,
+            TilingRuleOutput.Neighbor.This => tile != null,
+            _ => base.RuleMatch(neighbor, tile)
+        };
+
+        public class Neighbor : RuleTile.TilingRuleOutput.Neighbor
+        {
+            public const int Null = 3;
+            public const int NotNull = 4;
+        }
     }
 }
