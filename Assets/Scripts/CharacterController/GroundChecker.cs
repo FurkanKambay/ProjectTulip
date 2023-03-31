361e408326c3f3fb7d2e7f1cfc876e05430e5c0e
Furkan Kambay <furkankambay@protonmail.com>
2023-03-31 04:48:01 +0300

    Switch to using a circle collider for ground checking

--- Assets/Scripts/CharacterController/GroundChecker.cs
+++ Assets/Scripts/CharacterController/GroundChecker.cs
@@ -7,15 +7,15 @@ namespace Game.CharacterController
         public bool IsOnGround { get; private set; }
 
         [SerializeField] private LayerMask groundLayer;
-        [SerializeField] private Vector2 boxSize;
+        [SerializeField] private float size;
 
-        private void FixedUpdate() => IsOnGround = Physics2D.OverlapBox(
-            transform.position, boxSize, 0, groundLayer);
+        private void FixedUpdate() => IsOnGround = Physics2D.OverlapCircle(
+            transform.position, size, groundLayer);
 
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = IsOnGround ? Color.green : Color.red;
-            Gizmos.DrawWireCube(transform.position, boxSize);
+            Gizmos.DrawWireSphere(transform.position, size);
         }
     }
 }
