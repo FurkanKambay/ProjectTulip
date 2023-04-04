4446557c2a6cc77d3052ac1435f01cd5b9c93c24
Furkan Kambay <furkankambay@protonmail.com>
2023-04-04 17:53:19 +0300

    Go back to using box collider for ground checking

--- Assets/Scripts/CharacterController/GroundChecker.cs
+++ Assets/Scripts/CharacterController/GroundChecker.cs
@@ -7,15 +7,15 @@ namespace Game.CharacterController
         public bool IsOnGround { get; private set; }
 
         [SerializeField] private LayerMask groundLayer;
-        [SerializeField] private float size;
+        [SerializeField] private Vector2 boxSize;
 
-        private void FixedUpdate() => IsOnGround = Physics2D.OverlapCircle(
-            transform.position, size, groundLayer);
+        private void FixedUpdate() => IsOnGround = Physics2D.OverlapBox(
+            transform.position, boxSize, 0, groundLayer);
 
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = IsOnGround ? Color.green : Color.red;
-            Gizmos.DrawWireSphere(transform.position, size);
+            Gizmos.DrawWireCube(transform.position, boxSize);
         }
     }
 }
