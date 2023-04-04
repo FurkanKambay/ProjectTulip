d2ff497c49c90ff98b56c4d771defc3a7cf3222f
Furkan Kambay <furkankambay@protonmail.com>
2023-04-04 12:09:20 +0300

    Implement smooth camera follow

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -8,7 +8,8 @@ namespace Game.Player
     public class CameraFollow : MonoBehaviour
     {
         [SerializeField] private Transform player;
-        [SerializeField] private Vector2 offset;
+
+        public TrackingOptions tracking;
         public ZoomOptions zoom;
 
         private new Camera camera;
@@ -17,15 +18,40 @@ namespace Game.Player
 
         private void Update()
         {
+            tracking.Target = player.position + (Vector3)tracking.Offset;
             zoom.Target -= Mouse.current.scroll.y.value * zoom.Sensitivity;
         }
 
         private void LateUpdate()
         {
-            Transform cam = transform;
-            Vector3 center = player.position + (Vector3)offset;
-            cam.position = new Vector3(center.x, center.y, cam.position.z);
             camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, zoom.Target, Time.deltaTime * zoom.Speed);
+
+            Vector3 position = transform.position;
+            float lerpX = Mathf.Lerp(position.x, tracking.Target.x, Time.deltaTime * tracking.Speed.x);
+            float lerpY = Mathf.Lerp(position.y, tracking.Target.y, Time.deltaTime * tracking.Speed.y);
+
+            Vector3 distance = tracking.Target - position;
+            float targetX = Mathf.Abs(distance.x) < tracking.SnapValue ? tracking.Target.x : lerpX;
+            float targetY = Mathf.Abs(distance.y) < tracking.SnapValue ? tracking.Target.y : lerpY;
+            transform.position = new Vector3(targetX, targetY, tracking.Target.z);
+        }
+
+        [Serializable]
+        public class TrackingOptions : IValidate
+        {
+            private Vector3 target;
+
+            public Vector3 Target
+            {
+                get => target;
+                set => target = new Vector3(value.x, value.y, -10f);
+            }
+
+            [field: SerializeField] public Vector2 Offset { get; private set; }
+            [field: SerializeField] public Vector2 Speed { get; private set; } = Vector2.one * 10f;
+            [field: SerializeField] public float SnapValue { get; private set; } = .5f;
+
+            public void OnValidate() { }
         }
 
         [Serializable]
