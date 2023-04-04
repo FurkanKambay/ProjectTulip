b6851f84515443474b2fe7fbbd4c3f689b879e20
Furkan Kambay <furkankambay@protonmail.com>
2023-04-04 10:51:21 +0300

    Rename to zoom.Target

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using Game.Data;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -13,25 +14,29 @@ namespace Game.Player
         private new Camera camera;
 
         private void Awake() => camera = GetComponent<Camera>();
-        private void Update() => zoom.Value -= Mouse.current.scroll.y.value * zoom.Sensitivity;
+
+        private void Update()
+        {
+            zoom.Target -= Mouse.current.scroll.y.value * zoom.Sensitivity;
+        }
 
         private void LateUpdate()
         {
             Transform cam = transform;
             Vector3 center = player.position + (Vector3)offset;
             cam.position = new Vector3(center.x, center.y, cam.position.z);
-            camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, zoom.Value, Time.deltaTime * zoom.Speed);
+            camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, zoom.Target, Time.deltaTime * zoom.Speed);
         }
 
-        [System.Serializable]
+        [Serializable]
         public class ZoomOptions : IValidate
         {
-            [SerializeField] private float value = 10f;
+            [SerializeField] private float target = 10f;
 
-            public float Value
+            public float Target
             {
-                get => value;
-                set => this.value = Mathf.Clamp(value, Min, Max);
+                get => target;
+                set => this.target = Mathf.Clamp(value, Min, Max);
             }
 
             [field: SerializeField] public float Min { get; private set; } = 1f;
@@ -39,7 +44,7 @@ namespace Game.Player
             [field: SerializeField] public float Sensitivity { get; private set; } = .01f;
             [field: SerializeField] public float Speed { get; private set; } = 10f;
 
-            public void OnValidate() => Value = value;
+            public void OnValidate() => Target = target;
         }
     }
 }
