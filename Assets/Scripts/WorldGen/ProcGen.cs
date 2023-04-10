c3b26364072d6ecf0c8c9c9b9483ae8a4039c393
Furkan Kambay <furkankambay@protonmail.com>
2023-04-10 03:09:20 +0300

    Implement proc gen compute shader with parameters

--- Assets/Scripts/WorldGen/ProcGen.cs
+++ Assets/Scripts/WorldGen/ProcGen.cs
@@ -1,31 +1,69 @@
+using System;
 using UnityEngine;
 
 namespace Game.WorldGen
 {
     public class ProcGen : MonoBehaviour
     {
-        public ComputeShader shader;
-        public RenderTexture texture;
+        public ComputeShader computeShader;
 
-        public int width = 512;
-        public int height = 512;
+        [SerializeField, Range(1, 128)] private int xFactor;
+        [SerializeField, Range(1, 128)] private int yFactor;
+        [SerializeField, Range(8, 1024)] private int width = 128;
+        [SerializeField, Range(8, 1024)] private int height = 128;
+        [SerializeField, Range(0, 1)] private float wallCutoff = .5f;
+        [SerializeField] private int[] neighborCutoffSteps;
+
+        private ComputeBuffer buffer;
+        private float[] data;
 
         private void Start() => Generate();
-
-        private void SetShaderProps()
-        {
-            shader.SetTexture(0, "result", texture);
-            shader.SetFloat("resolution_x", width);
-            shader.SetFloat("resolution_y", height);
-            shader.Dispatch(0, width / 8, height / 8, 1);
-        }
 
         [ContextMenu("Generate")]
         private void Generate()
         {
-            texture = new RenderTexture(width, height, 24) { enableRandomWrite = true };
-            texture.Create();
-            SetShaderProps();
+            buffer = new ComputeBuffer(width * height, sizeof(int));
+
+            int indexMain = computeShader.FindKernel("cs_main");
+            computeShader.SetBuffer(indexMain, "result", buffer);
+            computeShader.SetFloat("width", width);
+            computeShader.SetFloat("height", height);
+            computeShader.SetFloat("wall_cutoff", wallCutoff);
+            computeShader.Dispatch(indexMain, width / 8, height / 8, 1);
+
+            int indexSmooth = computeShader.FindKernel("cs_smooth");
+            computeShader.SetBuffer(indexSmooth, "result", buffer);
+
+            for (int i = 0; i < neighborCutoffSteps?.Length; i++)
+            {
+                computeShader.SetInt("neighbor_cutoff", neighborCutoffSteps?[i] ?? 4);
+                computeShader.Dispatch(indexSmooth, width / 8, height / 8, 1);
+            }
+
+            buffer.GetData(data);
+            buffer.Release();
+        }
+
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < data?.Length; i++)
+            {
+                int x = i % width;
+                int y = i / width;
+                float value = data[i];
+
+                Gizmos.color = value == 0 ? Color.black : Color.white;
+                Gizmos.DrawCube(new Vector3(x, y, 0), Vector3.one * 1f);
+            }
+        }
+
+        private void OnValidate()
+        {
+            width = xFactor * 8;
+            height = yFactor * 8;
+
+            Array.Resize(ref data, width * height);
+            Generate();
         }
     }
 }
