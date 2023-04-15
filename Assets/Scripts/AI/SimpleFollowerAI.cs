3d2ba32bd64aaf181050c001d747f0ca12152e1a
Furkan Kambay <furkankambay@protonmail.com>
2023-04-15 06:41:41 +0300

    Give wizard a simple follower AI

---
+++ Assets/Scripts/AI/SimpleFollowerAI.cs
@@ -0,0 +1,25 @@
+﻿using Game.Data.Interfaces;
+using UnityEngine;
+
+namespace Game.AI
+{
+    [RequireComponent(typeof(IMovement))]
+    public class SimpleFollowerAI : MonoBehaviour
+    {
+        private Transform target;
+        private IMovement movement;
+
+        private void Awake()
+        {
+            movement = GetComponent<IMovement>();
+            if (!target) target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        private void Update()
+        {
+            if (!target || movement == null) return;
+            Vector3 distance = target.transform.position - transform.position;
+            movement.Input = Mathf.Abs(distance.x) < 1f ? Vector2.zero : distance.normalized;
+        }
+    }
+}
