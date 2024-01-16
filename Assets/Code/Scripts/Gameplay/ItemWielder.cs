3d3da789980a8a659f85922f3c0e1d126394067c
Furkan Kambay <furkankambay@protonmail.com>
2024-01-16 20:20:49 +0300

    Rework item/weapon swing system and use new sprites

---
+++ Assets/Code/Scripts/Gameplay/ItemWielder.cs
@@ -0,0 +1,126 @@
+using DG.Tweening;
+using Game.Data.Interfaces;
+using Game.Input;
+using Game.Player;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Game.Gameplay
+{
+    [RequireComponent(typeof(Inventory))]
+    public class ItemWielder : MonoBehaviour
+    {
+        public IUsable Current => inventory.HotbarSelected?.Item as IUsable;
+
+        [SerializeField] private Transform itemPivot;
+
+        [SerializeField] private float hideItemDelay = 2f;
+
+        [SerializeField] private float readyAngle = -10;
+
+        [SerializeField] private float chargeAngle = 45f;
+        [SerializeField] private float swingAngle = -90f;
+
+        [SerializeField] private float chargeDuration = 0.2f;
+        [SerializeField] private float swingDuration = 0.1f;
+
+        private ItemSwingState state;
+        private float timeSinceLastUse;
+
+        private Inventory inventory;
+        private Transform itemVisual;
+        private SpriteRenderer itemRenderer;
+
+        public void ChargeAndSwing(TweenCallback onCharge = null, TweenCallback onSwing = null)
+        {
+            if (timeSinceLastUse <= Current.Cooldown) return;
+            if (state != ItemSwingState.Ready) return;
+            timeSinceLastUse = 0f;
+
+            DoCharge(() =>
+            {
+                onCharge?.Invoke();
+                DoSwing(onSwing);
+            });
+        }
+
+        private void DoCharge(TweenCallback onComplete)
+        {
+            state = ItemSwingState.Charging;
+            itemVisual.DOLocalRotate(Vector3.forward * chargeAngle, chargeDuration)
+                .OnComplete(() =>
+                {
+                    state = ItemSwingState.Charged;
+                    onComplete?.Invoke();
+                });
+        }
+
+        private void DoSwing(TweenCallback onComplete)
+        {
+            state = ItemSwingState.Swinging;
+            itemVisual.DOLocalRotate(Vector3.forward * swingAngle, swingDuration)
+                .OnComplete(() =>
+                {
+                    onComplete?.Invoke();
+                    ResetState();
+                });
+        }
+
+        private void ResetState()
+        {
+            state = ItemSwingState.Resetting;
+            itemVisual.DOLocalRotate(Vector3.forward * readyAngle, swingDuration)
+                .OnComplete(() => state = ItemSwingState.Ready);
+        }
+
+        private void Update()
+        {
+            timeSinceLastUse += Time.deltaTime;
+
+            itemRenderer.enabled = timeSinceLastUse < hideItemDelay;
+
+            if (UnityEngine.InputSystem.Keyboard.current.hKey.wasPressedThisFrame)
+            {
+                Time.timeScale -= .05f;
+            }
+            if (UnityEngine.InputSystem.Keyboard.current.jKey.wasPressedThisFrame)
+            {
+                Time.timeScale += .05f;
+            }
+
+            if (state == ItemSwingState.Ready)
+            {
+                Vector2 deltaToMouse = InputHelper.Instance.MouseWorldPoint - (Vector2)transform.position;
+                float aimDirectionSign = Mathf.Sign(deltaToMouse.x);
+                itemPivot.localScale = new Vector3(aimDirectionSign < 0 ? -1 : 1, 1, 1);
+            }
+
+            if (InputHelper.Actions.Player.Use.inProgress)
+                ChargeAndSwing();
+        }
+
+        private void UpdateItemSprite(int _) => itemRenderer.sprite = inventory.HotbarSelected?.Item?.Icon;
+
+        private void Awake()
+        {
+            inventory = GetComponent<Inventory>();
+            itemRenderer = itemPivot.GetComponentInChildren<SpriteRenderer>();
+            itemVisual = itemRenderer.transform;
+            Assert.IsNotNull(itemPivot);
+
+            itemVisual.localEulerAngles = Vector3.forward * readyAngle;
+        }
+
+        private void OnEnable() => inventory.HotbarSelectionChanged += UpdateItemSprite;
+        private void OnDisable() => inventory.HotbarSelectionChanged -= UpdateItemSprite;
+    }
+
+    internal enum ItemSwingState
+    {
+        Ready,
+        Charging,
+        Charged,
+        Swinging,
+        Resetting
+    }
+}
--- Assets/Code/Scripts/Player/PlayerAnimator.cs
+++ Assets/Code/Scripts/Player/PlayerAnimator.cs
@@ -1,6 +1,5 @@
 using Game.CharacterController;
 using Game.Data.Interfaces;
-using Game.Input;
 using UnityEngine;
 
 namespace Game.Player
@@ -9,50 +8,20 @@ namespace Game.Player
     {
         private static readonly int animSpeed = Animator.StringToHash("speed");
         private static readonly int animJump = Animator.StringToHash("jump");
-        private static readonly int animAttack = Animator.StringToHash("attack");
-        private static readonly int animAttackSpeed = Animator.StringToHash("attack speed");
 
         private Animator animator;
-        private Inventory inventory;
-        private WorldModifier worldModifier;
         private IMovement movement;
         private PlayerJump playerJump;
 
-        private InputActions.PlayerActions playerActions;
-
         private void Awake()
         {
-            playerActions = InputHelper.Actions.Player;
-
             animator = GetComponent<Animator>();
-            inventory = GetComponentInParent<Inventory>();
-            worldModifier = GetComponentInParent<WorldModifier>();
             movement = GetComponentInParent<IMovement>();
             playerJump = GetComponentInParent<PlayerJump>();
         }
 
-        private void Start()
-        {
-            inventory.HotbarSelectionChanged += OnHotbarSelection;
-            OnHotbarSelection(inventory.HotbarSelectedIndex);
-        }
-
-        private void OnHotbarSelection(int index)
-        {
-            if (inventory.HotbarSelected?.Item is not IUsable item) return;
-            animator.SetFloat(animAttackSpeed, 1f / item.Cooldown);
-        }
-
         private void Update()
         {
-            bool canPlayAttack = inventory.HotbarSelected?.Item switch
-            {
-                ITool => worldModifier && worldModifier.FocusedCell.HasValue && playerActions.Use.inProgress,
-                Weapon => playerActions.Use.inProgress,
-                _ => false
-            };
-
-            animator.SetBool(animAttack, canPlayAttack);
             animator.SetFloat(animSpeed, Mathf.Abs(movement.Velocity.x));
             animator.SetBool(animJump, playerJump.CurrentlyJumping);
         }
