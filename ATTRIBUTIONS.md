d9883d6e48bc5110d937a58dc784c8d3ca4fa1e8
Furkan Kambay <furkankambay@protonmail.com>
2024-01-17 17:06:24 +0300

    Change enemy sprites to a skeleton

--- ATTRIBUTIONS.md
+++ ATTRIBUTIONS.md
@@ -6,6 +6,7 @@ These are used for prototyping purposes only.
 
 - "Hooded Protagonist" by Penzilla — https://penzilla.itch.io/hooded-protagonist
 - "Magic Wizard Protagonist" by Penzilla — https://penzilla.itch.io/magic-wizard
+- "Free Skeleton Pixel Art Sprite Sheets" by CraftPix.net — https://craftpix.net/freebies/free-skeleton-pixel-art-sprite-sheets
 - "Seamless HD landscape in parts" by PWL — https://opengameart.org/content/seamless-hd-landscape-in-parts
 - "Animated Axe Cursor" by smallmak — https://smallmak.itch.io/animated-axe-cursor
 
--- Assets/Code/Scripts/Gameplay/CharacterAnimator.cs
+++ Assets/Code/Scripts/Gameplay/CharacterAnimator.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using Game.Data.Interfaces;
+using UnityEngine;
 
 namespace Game.Gameplay
 {
@@ -9,8 +11,13 @@ namespace Game.Gameplay
         [SerializeField] AudioClip hurtSound;
         [SerializeField] AudioClip dieSound;
 
+        private IMovement movement;
+
+        private static readonly int animSpeed = Animator.StringToHash("speed");
         private static readonly int animHurt = Animator.StringToHash("hurt");
         private static readonly int animDie = Animator.StringToHash("die");
+
+        private void FixedUpdate() => animator.SetFloat(animSpeed, Mathf.Abs(movement.Velocity.x));
 
         private void OnDamageTaken(DamageEventArgs damage)
         {
@@ -35,6 +42,7 @@ namespace Game.Gameplay
         {
             health ??= GetComponent<Health>();
             animator ??= GetComponent<Animator>();
+            movement ??= GetComponent<IMovement>();
         }
 
         private void OnEnable()
