52c62ba5620ab00c01d86d63aab969df8de017bb
Furkan Kambay <furkankambay@protonmail.com>
2024-03-24 07:16:13 +0300

    Implement respawning

--- Assets/Code/Scripts/Gameplay/Respawner.cs
+++ Assets/Code/Scripts/Gameplay/Respawner.cs
@@ -1,3 +1,5 @@
+using Tulip.Data;
+using Tulip.Data.Gameplay;
 using UnityEngine;
 
 namespace Tulip.Gameplay
@@ -5,10 +7,47 @@ namespace Tulip.Gameplay
     [RequireComponent(typeof(Rigidbody2D))]
     public class Respawner : MonoBehaviour
     {
+        [SerializeField] bool autoRespawn = true;
+        [SerializeField] float respawnDelay;
         [SerializeField] Vector3 respawnPosition;
+
+        public float SecondsUntilRespawn { get; private set; }
+        public bool CanRespawn => SecondsUntilRespawn <= 0;
+
+        private IHealth health;
         private Rigidbody2D body;
 
-        private void Awake() => body = GetComponent<Rigidbody2D>();
-        public void Respawn() => body.MovePosition(respawnPosition);
+        private void Awake()
+        {
+            health = GetComponent<IHealth>();
+            body = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if (health.IsAlive) return;
+
+            SecondsUntilRespawn -= Time.deltaTime;
+            if (!CanRespawn) return;
+
+            if (autoRespawn)
+                Respawn();
+        }
+
+        [ContextMenu(nameof(Respawn))]
+        public void Respawn()
+        {
+            if (!CanRespawn) return;
+
+            health.Revive();
+            body.MovePosition(respawnPosition);
+
+            SecondsUntilRespawn = 0;
+        }
+
+        private void OnEnable() => health.OnDie += HandleDeath;
+        private void OnDisable() => health.OnDie -= HandleDeath;
+
+        private void HandleDeath(DamageEventArgs _) => SecondsUntilRespawn = respawnDelay;
     }
 }
