124b615ddcefbdfdb47405e20d9e29ecf124d71e
Furkan Kambay <furkankambay@protonmail.com>
2024-06-13 22:45:20 +0300

    feat: keep player within playground

---
+++ Assets/Code/MenuPlayground.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Tulip
+{
+    public class MenuPlayground : MonoBehaviour
+    {
+        [SerializeField] Transform player;
+        [SerializeField] new Camera camera;
+
+        private void Update()
+        {
+            Vector3 cameraCenter = camera.transform.position;
+            float cameraExtent = camera.orthographicSize * camera.aspect;
+            float left = cameraCenter.x - cameraExtent;
+            float right = cameraCenter.x + cameraExtent;
+
+            Vector3 targetPosition = player.position;
+
+            if (player.position.x > right)
+                targetPosition.x = left;
+            else if (player.position.x < left)
+                targetPosition.x = right;
+
+            player.position = targetPosition;
+        }
+    }
+}
