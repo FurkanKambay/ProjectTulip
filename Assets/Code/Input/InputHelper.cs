eeb46852191b4110133fa750cc2c5e42bf80ff5f
Furkan Kambay <furkankambay@protonmail.com>
2024-07-14 14:50:09 +0300

    refactor(core): make input helper into a MonoBehaviour

--- Assets/Code/Input/InputHelper.cs
+++ Assets/Code/Input/InputHelper.cs
@@ -4,40 +4,40 @@ using UnityEngine.InputSystem;
 
 namespace Tulip.Input
 {
-    public class InputHelper : ScriptableObject
+    public class InputHelper : MonoBehaviour
     {
-        private static InputHelper instance;
+        private void Awake()
+        {
+            GameState.OnGameStateChange += HandleGameStateChanged;
+            Debug.Log("[Input] Enabled input helper.");
+        }
 
         private static void HandleGameStateChanged()
         {
+            InputActionMap playerControls = InputSystem.actions.actionMaps[0];
+            InputActionMap uiControls = InputSystem.actions.actionMaps[1];
+
             if (GameState.Current.IsPlayerInputEnabled)
             {
                 Debug.Log($"[Input] + Player input enabled by {GameState.Current}.");
-                InputSystem.actions.actionMaps[0].Enable();
+                playerControls.Enable();
             }
             else
             {
                 Debug.Log($"[Input] - Player input disabled by {GameState.Current}.");
-                InputSystem.actions.actionMaps[0].Disable();
+                playerControls.Disable();
             }
 
             if (GameState.Current.IsUIInputEnabled)
             {
                 Debug.Log($"[Input] - UI input enabled by {GameState.Current}.");
-                InputSystem.actions.actionMaps[1].Enable();
+                uiControls.Enable();
             }
             else
             {
                 Debug.Log($"[Input] - UI input disabled by {GameState.Current}.");
-                InputSystem.actions.actionMaps[1].Disable();
+                uiControls.Disable();
             }
-        }
-
-        private void OnEnable()
-        {
-            instance = this;
-            GameState.OnGameStateChange += HandleGameStateChanged;
-            Debug.Log("[Input] Enabled input helper.");
         }
     }
 }
