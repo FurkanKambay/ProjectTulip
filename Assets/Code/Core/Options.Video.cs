8af93256b8f6d262a803e3cc2bd549e4b57e09d6
Furkan Kambay <furkankambay@protonmail.com>
2024-07-16 19:42:03 +0300

    fix: full screen mode switch issue

--- Assets/Code/Core/Options.Video.cs
+++ Assets/Code/Core/Options.Video.cs
@@ -11,68 +11,110 @@ namespace Tulip.Core
         [Serializable]
         public sealed record VideoOptions : ISerializationCallbackReceiver
         {
-            [SerializeField] FullScreenMode fullScreenMode;
-            [SerializeField] string resolution;
-
-            private List<string> supportedResolutions;
-
-            public List<string> SupportedResolutions
-            {
-                get
-                {
-                    if (supportedResolutions is not { Count: > 0})
-                    {
-                        supportedResolutions = Screen.resolutions
-                            .Select(r => $"{r.width}\u00d7{r.height}")
-                            .Reverse()
-                            .ToList();
-                    }
-
-                    return supportedResolutions;
-                }
-            }
-
-            private VideoOptions() { }
-
-            internal void LoadValues()
-            {
-                resolution = LoadOption(Keys.Resolution, GetDefaultResolution());
-                FullScreenMode = LoadOption(Keys.FullScreenMode, FullScreenMode.FullScreenWindow);
-            }
+            [SerializeField] FullScreenMode windowMode;
+            [SerializeField] Resolution resolution;
 
             [CreateProperty]
-            public FullScreenMode FullScreenMode
+            public string WindowMode
             {
-                get => fullScreenMode;
-                set => SetOption(Keys.FullScreenMode, ref fullScreenMode, value);
+                get => WindowModeToString(windowMode);
+                set
+                {
+                    FullScreenMode newMode = StringToWindowMode(value);
+                    Screen.fullScreenMode = newMode;
+                    SetOption(Keys.FullScreenMode, ref windowMode, newMode);
+                }
             }
 
             [CreateProperty]
             public string Resolution
             {
-                get => IsResolutionValid(resolution) ? resolution : resolution = GetDefaultResolution();
+                get
+                {
+                    if (!IsResolutionValid(resolution))
+                        resolution = StringToResolution(GetDefaultResolution());
+
+                    return ResolutionToString(resolution);
+                }
                 set
                 {
-                    if (!IsResolutionValid(value))
+                    Resolution newResolution = StringToResolution(value);
+
+                    if (!IsResolutionValid(newResolution))
                         return;
 
-                    string[] resolutionParts = Resolution.Split('\u00d7', 2);
-                    int width = int.Parse(resolutionParts[0]);
-                    int height = int.Parse(resolutionParts[1]);
-                    Screen.SetResolution(width, height, FullScreenMode);
-
-                    SetOption(Keys.Resolution, ref resolution, value);
+                    resolution = newResolution;
+                    Screen.SetResolution(resolution.width, resolution.height, windowMode);
+                    SetOption(Keys.Resolution, ref resolution, newResolution);
                 }
             }
 
+            [CreateProperty] List<string> supportedResolutions;
+
+            [CreateProperty] List<string> supportedWindowModes = new()
+            {
+                WindowModeToString(FullScreenMode.FullScreenWindow),
+                WindowModeToString(FullScreenMode.Windowed)
+            };
+
+            private VideoOptions()
+            {
+            }
+
+            internal void LoadValues()
+            {
+                supportedResolutions = Screen.resolutions
+                    .Select(ResolutionToString)
+                    .Reverse()
+                    .ToList();
+
+                Resolution = LoadOption(Keys.Resolution, GetDefaultResolution());
+                WindowMode = LoadOption(Keys.FullScreenMode, supportedWindowModes[0]);
+            }
+
+#region Converters
+
+            private static string WindowModeToString(FullScreenMode value) =>
+                windowModeLabels[value];
+
+            private static FullScreenMode StringToWindowMode(string value) =>
+                windowModeLabels.First(kvp => kvp.Value == value).Key;
+
+            private static string ResolutionToString(Resolution value) =>
+                $"{value.width}\u00d7{value.height}";
+
+            private static Resolution StringToResolution(string value)
+            {
+                string[] resolutionParts = value.Split('\u00d7', 2);
+                int width = int.Parse(resolutionParts[0]);
+                int height = int.Parse(resolutionParts[1]);
+
+                return new Resolution
+                {
+                    width = width,
+                    height = height
+                };
+            }
+
+#endregion
+
             private string GetDefaultResolution() =>
-                SupportedResolutions.Count > 0 ? SupportedResolutions[0] : string.Empty;
+                supportedResolutions.Count > 0 ? supportedResolutions[0] : string.Empty;
 
-            private bool IsResolutionValid(string value) =>
-                SupportedResolutions.Contains(value);
+            private bool IsResolutionValid(Resolution value) =>
+                supportedResolutions.Contains(ResolutionToString(value));
 
-            void ISerializationCallbackReceiver.OnBeforeSerialize() => resolution = Resolution;
+            void ISerializationCallbackReceiver.OnBeforeSerialize() => resolution = StringToResolution(Resolution);
             void ISerializationCallbackReceiver.OnAfterDeserialize() { }
+
+            // TODO: use Localization
+            private static readonly Dictionary<FullScreenMode, string> windowModeLabels = new()
+            {
+                [FullScreenMode.ExclusiveFullScreen] = "Full Exclusive",
+                [FullScreenMode.FullScreenWindow] = "Full Borderless",
+                [FullScreenMode.MaximizedWindow] = "Maximized",
+                [FullScreenMode.Windowed] = "Windowed"
+            };
 
             internal static class Keys
             {
--- Assets/Code/UI/SettingsPresenter.cs
+++ Assets/Code/UI/SettingsPresenter.cs
@@ -39,7 +39,6 @@ namespace Tulip.UI
         private Toggle quitFlyoutButton;
         private Button menuQuitButton;
         private Button gameExitButton;
-        private DropdownField resolutionDropdown;
 
         private void Awake()
         {
@@ -55,17 +54,13 @@ namespace Tulip.UI
             quitFlyoutButton = root.Q<Toggle>("QuitFlyoutButton");
             gameExitButton = root.Q<Button>("SaveExitButton");
             menuQuitButton = root.Q<Button>("QuitConfirmButton");
-            resolutionDropdown = root.Q<DropdownField>("VideoResolution");
 
             optionsButton.RegisterCallback<ChangeEvent<bool>>(HandleOptionsToggle);
             gameExitButton.RegisterCallback<ClickEvent>(HandleSaveExitClicked);
             menuQuitButton.RegisterCallback<ClickEvent>(HandleQuitClicked);
 
-            // UXML binding does not work
-            resolutionDropdown.choices = Options.Instance.Video.SupportedResolutions;
-
 #if UNITY_WEBGL
-            resolutionDropdown.RemoveFromHierarchy();
+            root.Q<DropdownField>("VideoResolution").RemoveFromHierarchy();
 #endif
         }
 
