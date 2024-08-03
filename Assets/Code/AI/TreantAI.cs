fe65a7e3131b1043b00ec4f9b5a201acfe5aafaf
Furkan Kambay <furkankambay@protonmail.com>
2024-08-03 04:07:02 +0300

    feat(gameplay): treants with branch weapon

---
+++ Assets/Code/AI/TreantAI.cs
@@ -0,0 +1,51 @@
+using SaintsField;
+using Tulip.Character;
+using Tulip.Data;
+using Tulip.Data.Gameplay;
+using UnityEngine;
+
+namespace Tulip.AI
+{
+    public class TreantAI : MonoBehaviour, IWielderBrain
+    {
+        [Header("References")]
+        [SerializeField, Required] Health health;
+
+        [Header("Movement")]
+        [SerializeField] Vector2 attackDistance;
+
+        public Vector2 AimPosition { get; private set; }
+        public bool WantsToUse { get; private set; }
+
+        private Transform target;
+        private Health targetHealth;
+
+        private void OnEnable() => health.OnHurt += HandleHurt;
+        private void OnDisable() => health.OnHurt -= HandleHurt;
+
+        private void HandleHurt(HealthChangeEventArgs damage)
+        {
+            if (target)
+                return;
+
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+            targetHealth = target.GetComponentInChildren<Health>();
+        }
+
+        private void Update()
+        {
+            if (!health || health.IsDead || !targetHealth || targetHealth.IsDead)
+            {
+                WantsToUse = false;
+                return;
+            }
+
+            Vector2 targetVector = AimPosition - (Vector2)transform.position;
+            bool reachedX = Mathf.Abs(targetVector.x) < attackDistance.x;
+            bool reachedY = Mathf.Abs(targetVector.y) < attackDistance.y;
+
+            AimPosition = targetHealth.transform.position;
+            WantsToUse = reachedX && reachedY;
+        }
+    }
+}
