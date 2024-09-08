0f43021b0fc546d6f08c21527fddd147a3e894ee
Furkan Kambay <furkankambay@protonmail.com>
2024-09-08 21:55:55 +0300

    feat(audio): muffle music when indoors

--- Assets/Code/Audio/BiomeMusic.cs
+++ Assets/Code/Audio/BiomeMusic.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using FMOD.Studio;
 using FMODUnity;
+using SaintsField;
+using Tulip.GameWorld;
 using UnityEngine;
 
 namespace Tulip.Audio
@@ -10,11 +12,15 @@ namespace Tulip.Audio
         [Header("FMOD Events")]
         [SerializeField] EventReference biomeMusicEvent;
 
+        [Header("References")]
+        [SerializeField, Required] EntityLocationDeterminer playerLocation;
+
         [Header("Config")]
         [SerializeField] Biome startingBiome;
 
         private EventInstance musicInstance;
         private PARAMETER_DESCRIPTION paramBiome;
+        private PARAMETER_DESCRIPTION paramPlayerLocation;
 
         private IEnumerator Start()
         {
@@ -23,11 +29,15 @@ namespace Tulip.Audio
 
             EventDescription musicDescription = RuntimeManager.GetEventDescription(biomeMusicEvent);
             musicDescription.getParameterDescriptionByName("Biome", out paramBiome);
+            musicDescription.getParameterDescriptionByName("Player Location", out paramPlayerLocation);
             musicDescription.createInstance(out musicInstance);
 
             SetBiome(startingBiome);
             musicInstance.start();
         }
+
+        private void Update() =>
+            musicInstance.setParameterByID(paramPlayerLocation.id, playerLocation.Location.GetHashCode());
 
         private void SetBiome(Biome biome) =>
             musicInstance.setParameterByID(paramBiome.id, (float)biome);
